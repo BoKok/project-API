502afb5d0085f4f3136bf19363a269400db3b481
BoKok <kokbo@yahoo.com>
2021-12-01 15:48:06 -0600

    Revised

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,98 +1,125 @@
-var getCurrencyData = function () {
-    var apiUrl = 'https://free.currconv.com/api/v7/convert?q=USD_EUR&compact=ultra&apiKey=56ae4aece0ac2ac705cf';
+// association with static parent html container
+const rowContainer = document.getElementById("sacrifice");
+
+//global variables
+var dealData = [];
+var filteredData = [];
+var currencyData = [];
+
+var getGameData = function () {
+  var apiUrl = 'https://www.cheapshark.com/api/1.0/deals?storeID=1';
+
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+
+        response.json().then(function (data) {
+          
+          dealData = data;
+
+          var apiUrl = 'https://free.currconv.com/api/v7/convert?q=USD_EUR&compact=ultra&apiKey=56ae4aece0ac2ac705cf';
+
+          fetch(apiUrl)
+            .then(function (response) {
+
+              response.json().then(function (data2) {
+                currencyData = data2;
+
+
+                //loop through each array item
+                for (i = 0; i < dealData.length; i++) {
+                  //create html for game cards
+
+                  const gameRow = document.createElement("div")
+                  gameRow.setAttribute("class", "level");
+                  //column  class
+                  gameRow.classList.add("column");
+                  gameRow.classList.add("level-" + i)
+
+                  const gameCard = document.createElement("div");
+                  gameCard.setAttribute("class", "gameCard");
+                  const gameTitle = document.createElement("h2");
+                  const steamLink = document.createElement("a");
+                  const thumbNail = document.createElement("img");
+                  const priceDiv = document.createElement("div");
+                  const priceUSD = document.createElement("p");
+                  const priceEUR = document.createElement("p");
+                  const rating = document.createElement("p");
+                  const metaLink = document.createElement("a");
+                  const buildMetaLink = "https://www.metacritic.com" + dealData[i].metacriticLink;
+                  const buildSteamLink = "https://store.steampowered.com/app/" + dealData[i].steamAppID;
+
+                  //populate response data into these elements
+                  //steam links
+                  steamLink.innerHTML = dealData[i].title;
+                  steamLink.setAttribute("href", buildSteamLink);
+                  steamLink.setAttribute("target", "_blank");
+                  gameTitle.setAttribute("class", "noDecoration");
+                  //Metacritic links
+                  metaLink.innerHTML = "Metacritic Review";
+                  metaLink.setAttribute("href", buildMetaLink);
+                  metaLink.setAttribute("target", "_blank");
+                  
+                  let currentThumb = dealData[i].thumb;
+                  thumbNail.setAttribute("src", currentThumb);
+                  priceUSD.innerHTML = "$" + dealData[i].salePrice;
+                  //convert USD to EUR using free currency converter API
+                  priceEUR.innerHTML = "€" + (dealData[i].salePrice * currencyData.USD_EUR).toFixed(2);
+                  rating.innerHTML = "Steam Rating: " + dealData[i].steamRatingPercent + "%";
+
+                  //column formatting
+                  const row = document.createElement("div");
+                  if (i % 3 === 0) {
+                    row.classList.add("columns");
+                    row.classList.add("column-" + i);
+                    row.classList.add("column-" + (i + 1));
+                    row.classList.add("column-" + (i + 2));
+                    rowContainer.append(row)
+                  }
+
+                  $(".column-" + i).append($(".level-" + i));
+                  gameRow.append(gameCard);
+                  gameCard.append(gameTitle);
+                  gameTitle.append(steamLink);
+                  gameCard.append(thumbNail);
+                  gameCard.append(priceDiv);
+                  priceDiv.append(priceUSD);
+                  priceDiv.append(priceEUR);
+                  gameCard.append(rating);
+                  gameCard.append(metaLink);
+                  $(".column-" + i).append(gameRow);
+                  gameCard.classList.add("has-background-grey-lighter");
+                  $("h2").addClass("title is-4");
+                  $("p").addClass("is-size-5");
+                  $("p").addClass("has-text-weight-semibold");
+
+                }
+              });
+
+            });
+
+        });
+      }
+    })
+
+
+};
+
+getGameData();
+
+
+var rHistory = function(historyName) {
+  $("#searchBar").val(historyName);
+  gameSearch();
+}
+
+var gameSearch = function () {
+  //search bar/btn setup with html and onclick event
+  var searchBar = document.getElementById("searchBar")
+  var searchValue = searchBar.value;
+
+  //save input to localStorage
   
-    fetch(apiUrl)
-      .then(function (response) {
-        if (response.ok) {
-          response.json().then(function (data) {
-            console.log(data);
-            
-          });
-        } else {
-          
-        }
-      })
-      .catch(function (error) {
-        
-      });
-  };
-
- // getCurrencyData();
-
-const rowContainer = document.getElementById("row-container");
-
-  var getGameData= function () {
-    var apiUrl = 'https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15&';
-  
-<<<<<<< Updated upstream
-    fetch(apiUrl)
-      .then(function (response) {
-        if (response.ok) {
-          
-          response.json().then(function (data) {
-            console.log(data);
-            //loop through each array item
-
-            for (i=0; i < data.length; i++) {
-              //create html for game cards
-              const gameRow = document.createElement("div")
-              gameRow.setAttribute("class","level");
-              //column  class
-              gameRow.classList.add("column");
-              gameRow.classList.add("level-" + i)
-
-              const gameCard = document.createElement("div");
-              gameCard.setAttribute("class", "gameCard");
-              const gameTitle = document.createElement("h2");
-              const thumbNail = document.createElement("img");
-              const priceDiv = document.createElement("div");
-              const priceUSD = document.createElement("p");
-              const priceEUR = document.createElement("p");
-              const rating = document.createElement("p");
-
-
-              //populate response data into these elements
-              gameTitle.innerHTML = data[i].title;
-              let currentThumb = data[i].thumb;
-              thumbNail.setAttribute("src", currentThumb);
-              priceUSD.innerHTML = "$" + data[i].salePrice;
-              priceEUR.innerHTML = "Euro placeholder"
-              rating.innerHTML = "Steam Rating: " + data[i].steamRatingPercent + "%";
-              
-              //column formatting
-              const row = document.createElement("div");
-              if (i % 3 === 0) {
-                row.classList.add("columns");
-                row.classList.add("column-" + i);
-                row.classList.add("column-" + (i + 1));
-                row.classList.add("column-" + (i + 2));
-                rowContainer.append(row)
-              }
-
-              $(".column-" + i).append($(".level-" + i));
-              gameRow.append(gameCard);
-              gameCard.append(gameTitle);
-              gameCard.append(thumbNail);
-              gameCard.append(priceDiv);
-              priceDiv.append(priceUSD);
-              priceDiv.append(priceEUR);
-              gameCard.append(rating);
-              $(".column-" + i).append(gameRow);
-
-            }
-
-          });
-        } else {
-          
-        }
-      })
-      .catch(function (error) {
-        
-      });
-  };
-
-  getGameData();
-=======
   if(localStorage.getItem(searchValue) === null) {
   localStorage.setItem(searchValue, gameSearch);
   const historyHeader = $("<h4>");
@@ -142,8 +169,8 @@ const rowContainer = document.getElementById("row-container");
       const priceEUR = document.createElement("p");
       const rating = document.createElement("p");
       const metaLink = document.createElement("a");
-      const buildMetaLink = "https://www.metacritic.com" + dealData[i].metacriticLink;
-      const buildSteamLink = "https://store.steampowered.com/app/" + dealData[i].steamAppID;
+      const buildMetaLink = "https://www.metacritic.com" + filteredData[i].metacriticLink;
+      const buildSteamLink = "https://store.steampowered.com/app/" + filteredData[i].steamAppID;
 
       //populate response data into these elements
       //steam links
@@ -223,4 +250,3 @@ for(i = 0; i < localStorage.length; i++) {
 
 }
 console.log(localStorage.length)
->>>>>>> Stashed changes
